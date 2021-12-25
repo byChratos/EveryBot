eb7fb2bc19e64fe313ff03cf0c63ff8712dd1858
Christian <62462799+byChratos@users.noreply.github.com>
2021-12-25 03:26:24 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token, activity, commandChannel } = require('./config.json');
-//const Sequelize = require('sequelize');
+const { Sequelize, QueryTypes } = require('sequelize');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+});
 
 client.commands = new Collection();
 client.cooldowns = new Collection();
@@ -22,16 +25,15 @@ for (const folder of commandFolders){
     }
 
 }
-/*
-// Database
+
+//Database connection
 const sequelize = new Sequelize('database', 'user', 'password', {
     host: 'localhost',
-    dialect: 'sqlite',
-    logging: false,
+    dialect:  'sqlite',
     storage: 'database.db',
-})
-*/
+});
 
+//Startup
 client.once('ready', () => {
 	console.log('Ready!');
 	client.user.setActivity(activity);
@@ -39,17 +41,110 @@ client.once('ready', () => {
 client.on("warn", console.warn);
 client.on("error", console.error);
 
+//Reaction role adding roles
+client.on('messageReactionAdd', async (reaction, user) => {
+
+    //Exact code from discord.js v13 guide
+    // When a reaction is received, check if the structure is partial
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message:', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
+
+    if(user.bot) return;
+
+    await sequelize.query(`SELECT id AS id FROM roles_Messages WHERE message_id = ?`, {
+        type: QueryTypes.SELECT,
+        logging: false,
+        replacements: [reaction.message.id]
+    }).then(result => {
+        if(result.length == 0) return;
+
+        const id = result[0].id;
+        sequelize.query(`SELECT role_id AS role_id FROM roles_EmojiForRole WHERE id = ? AND emoji_id = ?`, {
+            type: QueryTypes.SELECT,
+            logging: false,
+            replacements: [id, reaction.emoji.id]
+        }).then(second_result => {
+            if(second_result.length == 0) return;
+            
+            const role_id = second_result[0].role_id;
+            let role = reaction.message.guild.roles.cache.get(role_id);
+
+            reaction.message.guild.members.fetch(user.id)
+                .then(member => {
+                    member.roles.add(role).catch(console.error);
+                });
+
+        })
+    });
+
+});
+
+//Reaction roles remove roles
+client.on('messageReactionRemove', async (reaction, user) => {
+
+    //Exact code from discord.js v13 guide
+    // When a reaction is received, check if the structure is partial
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message:', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+	}
+
+    if(user.bot) return;
+
+    await sequelize.query(`SELECT id AS id FROM roles_Messages WHERE message_id = ?`, {
+        type: QueryTypes.SELECT,
+        logging: false,
+        replacements: [reaction.message.id]
+    }).then(result => {
+        if(result.length == 0) return;
+
+        const id = result[0].id;
+        sequelize.query(`SELECT role_id AS role_id FROM roles_EmojiForRole WHERE id = ? AND emoji_id = ?`, {
+            type: QueryTypes.SELECT,
+            logging: false,
+            replacements: [id, reaction.emoji.id]
+        }).then(second_result => {
+            if(second_result.length == 0) return;
+            
+            const role_id = second_result[0].role_id;
+            let role = reaction.message.guild.roles.cache.get(role_id);
+
+            reaction.message.guild.members.fetch(user.id)
+                .then(member => {
+                    member.roles.remove(role).catch(console.error);
+                });
+
+        })
+    });
+
+});
+
+//Interactions
 client.on('interactionCreate', async interaction => {
 	//Returns if interaction is not a command
 	if(!interaction.isCommand()) return;
 
-	//Returns if interaction is triggered by bot, not sure if possible lol
+	//Returns if interaction is triggered by bot, not sure if it is possible or not lol
 	if(interaction.user.bot) return;
 
     const command = client.commands.get(interaction.commandName);
 
 	// Message if the command is in a wrong channel
-    if (command.name !== "clear"){
+    if (command.cmdChannel){
         if (commandChannel !== "none" && interaction.channel.type !== 'dm'){
             if (commandChannel !== interaction.channel.id.toString()){
                 client.channels.fetch(commandChannel)
